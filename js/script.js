8a5296fc36df93dd378b9662365e72cabe0314a7
ChriEffe <christianferrazzano93@gmail.com>
2021-12-14 17:27:05 +0100

    foreach

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-/*Scrivere una funzione (e lanciarla :razzo:) che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+/*Scrivere una funzione (e lanciarla) che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.*/
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const a = 3;
+const b = 8;
+
+let newArray = middleArray(array, a, b);
+console.log(newArray);
+
+function middleArray(array, min, max) {
+        const newArray = [];
+        array.forEach((num, i) => {
+            if (i > min && i < max) {
+                newArray.push(num);
+            }
+        });
+        return newArray
+}
